f72c54dae14628e2a555760fac89833df0029b19
kaveeshanethruwan <kaveesha.james151@gmail.com>
2024-07-08 20:46:21 +0530

    add useQuery basic pattern

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,7 +1,24 @@
+import axios from 'axios'
 import React from 'react'
+import { useQuery } from 'react-query'
+
+const fetchSuperHeroes = () => {
+    return axios.get('http://localhost:4000/superheroes')
+}
 
 export const RQSuperHeroesPage = () => {
+    const { isLoading, data } = useQuery('super-heroes', fetchSuperHeroes)
+
+    if (isLoading) return <h2>Loading...</h2>
+
     return (
-        <div>RQSuperHeroesPage</div>
+        <>
+            <h2>RQ Super Heroes Page</h2>
+            {
+                data?.data.map((item) => (
+                    <div key={item.name}>{item.name}</div>
+                ))
+            }
+        </>
     )
 }
